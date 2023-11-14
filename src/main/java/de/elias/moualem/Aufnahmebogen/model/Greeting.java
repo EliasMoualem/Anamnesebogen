499a94ffc438e1e0d35fb189da4552878818f548
Elias <elias.moualem@gmail.com>
2023-11-14 13:42:56 +0100

    added date to template

--- src/main/java/de/elias/moualem/Aufnahmebogen/model/Greeting.java
+++ src/main/java/de/elias/moualem/Aufnahmebogen/model/Greeting.java
@@ -2,12 +2,17 @@ package de.elias.moualem.Aufnahmebogen.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
 
 @Setter
 @Getter
 public class Greeting {
 
-    private long id;
-    private String content;
+    private String lastName;
+    private String firstName;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date birthDate;
 
 }
