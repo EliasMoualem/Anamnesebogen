58b0a5fb357dd4290bd3df5b196ad16eb1539166
Elias <elias.moualem@gmail.com>
2023-11-17 23:37:21 +0100

    expanded Patient.java and the form template

--- src/main/java/de/elias/moualem/Aufnahmebogen/model/Patient.java
+++ src/main/java/de/elias/moualem/Aufnahmebogen/model/Patient.java
@@ -11,4 +11,10 @@ public class Patient {
     private String firstName;
     private String birthDate;
     private String gender;
+    private String street;
+    private String zipCode;
+    private String city;
+    private String mobileNumber;
+    private String phoneNumber;
+    private String emailAddress;
 }
