06884ba7e3ae0b6e9e2dfdb75d88e26b0b176229
Elias <elias.moualem@gmail.com>
2023-12-10 09:53:30 +0100

    added thymeleaf config

---
+++ src/main/java/de/elias/moualem/Aufnahmebogen/config/ThymeleafConfig.java
@@ -0,0 +1,20 @@
+package de.elias.moualem.Aufnahmebogen.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafConfig {
+
+    @Bean
+    public ClassLoaderTemplateResolver bogenTemplateResolver(){
+        ClassLoaderTemplateResolver pdfTemplateResolver = new ClassLoaderTemplateResolver();
+        pdfTemplateResolver.setPrefix("templates/");
+        pdfTemplateResolver.setSuffix(".html");
+        pdfTemplateResolver.setTemplateMode("HTML");
+        pdfTemplateResolver.setCharacterEncoding("UTF-8");
+        pdfTemplateResolver.setOrder(1);
+        return pdfTemplateResolver;
+    }
+}
